4f60f9cfcf3fdffc734999b2ff17573c7f0c2f78
busenurdmb <bus.dmb@gmail.com>
2024-07-30 22:22:39 +0300

    Basket Mikroservisi oluşturuldu redis eklendi . dosyaları oluşturulup konfigürasyonları yapıldı ve bitti. Commentmikroservisi oluşturuldu

---
+++ Services/Basket/TakeAwayNight.Basket/LoginServices/LoginService.cs
@@ -0,0 +1,14 @@
+﻿namespace TakeAwayNight.Basket.LoginServices
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public LoginService(IHttpContextAccessor contextAccessor)
+        {
+            _httpContextAccessor = contextAccessor;
+        }
+        //giriş yapan kullanıcının userıd sini alma
+        //İİD BİGİSİ JWT SUB KEYWORDUNDE TUTULUYOR.
+        public string GetUserId => _httpContextAccessor.HttpContext.User.FindFirst("sub").Value;
+    }
+}
